119647ae0c69357baee92b6c4f3505c9f616cfe3
Chris Stoddart <cstoddart@rocketmail.com>
2018-03-21 19:33:59 -0500

    added 403 error example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/* global firebase */
+/* global firebaseui */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -20,3 +23,129 @@ ReactDOM.render(
 );
 
 registerServiceWorker();
+
+var config = {
+  apiKey: "AIzaSyBET2u2IqDZltYKApy0dHwOOqENlNH5iro",
+  authDomain: "cstoddart-meeting-scheduler.firebaseapp.com",
+  databaseURL: "https://cstoddart-meeting-scheduler.firebaseio.com",
+  projectId: "cstoddart-meeting-scheduler",
+  storageBucket: "cstoddart-meeting-scheduler.appspot.com",
+  messagingSenderId: "394986955881",
+  clientId: "290258087421-l8cck249qan0k2nbealve3d4q3h1i52g.apps.googleusercontent.com",
+  scopes: [
+    "email",
+    "profile",
+    "https://www.googleapis.com/auth/calendar",
+  ],
+  discoveryDocs: [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ]
+};
+
+
+firebase.initializeApp(config);
+
+var uiConfig = {
+  signInSuccessUrl: "localhost:5000", // Assuming you are running on your local machine
+  signInOptions: [
+    {
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      scopes: config.scopes
+    }
+  ],
+  // Terms of service url.
+  tosUrl: "<your-tos-url>"
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start("#firebaseui-auth-container", uiConfig);
+
+window.gapi.load('client:auth2', {
+  callback: function() {
+    // Handle gapi.client initialization.
+    console.log('callback...');
+    window.gapi.client
+        .init({
+          apiKey: config.apiKey,
+          clientId: config.clientID,
+          discoveryDocs: config.discoveryDocs,
+          scope: config.scopes.join(" ")
+        })
+        // Loading is finished, so start the app
+        .then(function() {
+          // Make sure the Google API Client is properly signed in
+          // if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
+          //   startApp();
+          // } else {
+          //   console.log('signing out...');
+          //   firebase.auth().signOut(); // Something went wrong, sign out
+          // }
+          startApp();
+        });
+  },
+  onerror: function() {
+    // Handle loading error.
+    alert('gapi.client failed to load!');
+  },
+  timeout: 5000, // 5 seconds.
+  ontimeout: function() {
+    // Handle timeout.
+    alert('gapi.client could not load in a timely manner!');
+  }
+});
+
+// This function will trigger when there is a login event
+firebase.auth().onAuthStateChanged(function(user) {
+  console.log(user)
+  // Make sure there is a valid user object
+  if (user) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://apis.google.com/js/api.js";
+    // Once the Google API Client is loaded, you can run your code
+    script.onload = function(e) {
+      // Initialize the Google API Client with the config object
+      // window.gapi.client
+      //   .init({
+      //     apiKey: config.apiKey,
+      //     clientId: config.clientID,
+      //     discoveryDocs: config.discoveryDocs,
+      //     scope: config.scopes.join(" ")
+      //   })
+      //   // Loading is finished, so start the app
+      //   .then(function() {
+      //     // Make sure the Google API Client is properly signed in
+      //     if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
+      //       startApp(user);
+      //     } else {
+      //       firebase.auth().signOut(); // Something went wrong, sign out
+      //     }
+      //   });
+    };
+    // Add to the document
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+});
+
+function startApp(user) {
+  console.log('user@startApp()', user);
+
+  // Make sure to refresh the Auth Token in case it expires!
+  firebase.auth().currentUser.getToken()
+  .then(function(){
+   return window.gapi.client.calendar.events
+    .list({
+      calendarId: "primary",
+      timeMin: new Date().toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      maxResults: 10,
+      orderBy: "startTime"
+    })
+  })
+  .then(function(response) {
+    console.log(response);
+  });
+}
